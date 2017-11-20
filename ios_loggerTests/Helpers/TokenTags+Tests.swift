89b38f7427bb6c8fe163bf4480a04ddb1c4f3311
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-11-20 13:26:28 +0300

    Implemented extensions

---
+++ ios_loggerTests/Helpers/TokenTags+Tests.swift
@@ -0,0 +1,21 @@
+//
+//  TokenTags+Tests.swift
+//  ios_loggerTests
+//
+//  Created by Evgeniy Akhmerov on 20/11/2017.
+//  Copyright © 2017 e-Legion. All rights reserved.
+//
+
+import ios_logger
+
+extension Token {
+    static let response = Token(rawValue: "Response.Token")!
+    static let error = Token(rawValue: "Error.Token")!
+    static let UI = Token(rawValue: "UI.Token")!
+}
+
+extension Tag {
+    static let client = Tag(rawValue: "Client.Tag")!
+    static let server = Tag(rawValue: "Server.Tag")!
+    static let `internal` = Tag(rawValue: "Internal.Tag")!
+}
