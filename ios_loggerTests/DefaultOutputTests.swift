40609298a0a3722be1fbe9516b5b99a7fca716a8
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-11-20 13:06:20 +0300

    Implemented blank files for tests

---
+++ ios_loggerTests/DefaultOutputTests.swift
@@ -0,0 +1,20 @@
+//
+//  Created by Evgeniy Akhmerov on 20/11/2017.
+//  Copyright © 2017 e-Legion. All rights reserved.
+//
+
+import XCTest
+import ios_logger
+
+class DefaultOutputTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+}
