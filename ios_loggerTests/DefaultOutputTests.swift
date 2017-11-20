b2709c2da2eb2a31f4f5088c9f74546cb0ac1343
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-11-20 18:32:43 +0300

    Implemented default output test

--- ios_loggerTests/DefaultOutputTests.swift
+++ ios_loggerTests/DefaultOutputTests.swift
@@ -4,17 +4,65 @@
 //
 
 import XCTest
-import ios_logger
+@testable import ios_logger
 
 class DefaultOutputTests: XCTestCase {
+    
+    private struct InnerMessage: Message {
+        let token: Token
+        let tags: [Tag]
+        let payload: String
+    }
+    
+    private class TestingDefaultOutput: DefaultOutput {
+        
+        private(set) var messagesDisplayed = 0
+        
+        override func display(_ message: Message) {
+            super.display(message)
+            
+            messagesDisplayed += 1
+        }
+    }
+    
+    private struct Constants {
+        enum Message: String {
+            case msg1 = "UnitTest.DefaultOutputTests.msg1"
+            case msg2 = "UnitTest.DefaultOutputTests.msg2"
+            case msg3 = "UnitTest.DefaultOutputTests.msg3"
+            case msg4 = "UnitTest.DefaultOutputTests.msg4"
+            case msg5 = "UnitTest.DefaultOutputTests.msg5"
+        }
+    }
+    
+    private var messages = [Message]()
     
     override func setUp() {
         super.setUp()
         
+        let msg1 = InnerMessage(token: .request, tags: [], payload: Constants.Message.msg1.rawValue)
+        let msg2 = InnerMessage(token: .response, tags: [], payload: Constants.Message.msg2.rawValue)
+        let msg3 = InnerMessage(token: .request, tags: [], payload: Constants.Message.msg3.rawValue)
+        let msg4 = InnerMessage(token: .UI, tags: [], payload: Constants.Message.msg4.rawValue)
+        let msg5 = InnerMessage(token: .error, tags: [], payload: Constants.Message.msg5.rawValue)
+        
+        messages = [msg1, msg2, msg3, msg4, msg5]
     }
     
-    override func tearDown() {
+    func testFullDisplaying() {
+        let output = TestingDefaultOutput()
         
-        super.tearDown()
+        messages.forEach { output.display($0) }
+        
+        XCTAssertEqual(output.messagesDisplayed, messages.count)
+    }
+    
+    func testBlockingMessages() {
+        let output = TestingDefaultOutput()
+        output.set(enabled: false, for: .request)
+        
+        messages.forEach { output.display($0) }
+        
+        XCTAssertEqual(output.messagesDisplayed, messages.count - 2)
     }
 }
