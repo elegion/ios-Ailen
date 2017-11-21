29d7671a9f4adbccc17bf3e485fd17feae431410
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-11-21 12:16:36 +0300

    Implemented logger tests

--- ios_loggerTests/AilenTests.swift
+++ ios_loggerTests/AilenTests.swift
@@ -4,17 +4,115 @@
 //
 
 import XCTest
-import ios_logger
+@testable import ios_logger
 
 class AilenTests: XCTestCase {
+        
+    private struct Constants {
+        static let msg = "Mock testing string"
+    }
+    
+    private var logger: Ailen?
     
     override func setUp() {
         super.setUp()
         
+        logger = Ailen()
     }
     
     override func tearDown() {
-        
         super.tearDown()
+        
+        logger = nil
+    }
+    
+    func testDefaultOutput() {
+        guard let logger = logger else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertEqual(logger.outputs.count, 1)
+        
+        let newOutput = DefaultOutput()
+        logger.outputs = [newOutput]
+        
+        XCTAssertEqual(logger.outputs.count, 1)
+        
+        logger.outputs.removeAll()
+
+        XCTAssertEqual(logger.outputs.count, 0)
+    }
+    
+    func testDefaultProcessor() {
+        guard let logger = logger else {
+            XCTFail()
+            return
+        }
+        
+        let logExpectation = expectation(description: "Async logging waiting")
+        
+        let output = TestingDefaultOutput()
+        logger.outputs = [output]
+        
+        logger.log(as: .UI, values: [Constants.msg])
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            XCTAssertEqual(logger.processors.count, 0)
+            XCTAssertEqual(output.messagesDisplayed, 1)
+
+            let processor = TestingProcessor()
+            logger.processors = [processor]
+
+            logger.log(as: .UI, values: [Constants.msg])
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                XCTAssertEqual(logger.processors.count, 1)
+                XCTAssertEqual(output.messagesDisplayed, 2)
+
+                logger.processors.removeAll()
+
+                logger.log(as: .UI, values: [Constants.msg])
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    XCTAssertEqual(logger.processors.count, 0)
+                    XCTAssertEqual(output.messagesDisplayed, 3)
+                    logExpectation.fulfill()
+                }
+            }
+        }
+
+        wait(for: [logExpectation], timeout: 10)
+    }
+    
+    func testEnablingDisablingToken() {
+        guard let logger = logger else {
+            XCTFail()
+            return
+        }
+        
+        let output = TestingDefaultOutput()
+        logger.outputs = [output]
+        XCTAssertEqual(logger.outputs.count, 1)
+        
+        let logExpectation = expectation(description: "Async logging waiting")
+        let token = Token.UI
+        
+        logger.log(as: token, values: [Constants.msg])
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            XCTAssertEqual(output.messagesDisplayed, 1)
+            
+            logger.set(enabled: false, for: token)
+            logger.log(as: token, values: [Constants.msg])
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                XCTAssertEqual(output.messagesDisplayed, 1)
+
+                logger.set(enabled: true, for: token)
+                logger.log(as: token, values: [Constants.msg])
+
+                XCTAssertEqual(output.messagesDisplayed, 2)
+                logExpectation.fulfill()
+            }
+        }
+        
+        wait(for: [logExpectation], timeout: 10)
     }
 }
