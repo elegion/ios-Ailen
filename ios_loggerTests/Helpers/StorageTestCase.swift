8281231a39f6cdc88ce719c270ca1abce4fd556a
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-11-30 11:16:54 +0300

    Implemented temporary solution to clean temp store

--- ios_loggerTests/Helpers/StorageTestCase.swift
+++ ios_loggerTests/Helpers/StorageTestCase.swift
@@ -23,16 +23,52 @@ class StorageTestCase: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        let paths = try? FileManager.default.contentsOfDirectory(atPath: NSTemporaryDirectory())
-        let mapped = paths?.flatMap({
-            guard $0.contains(Constants.dataStorageName) else { return nil }
-            return $0
-        })
-        mapped?.forEach({ try? FileManager.default.removeItem(at: URL(string: $0)!) })
+        print(NSTemporaryDirectory())
+        cleanStore()
         
         let path = NSTemporaryDirectory().appending(Constants.dataStorageName + ".sqlite")
         if FileManager.default.createFile(atPath: path, contents: nil, attributes: nil) {
             testModelPath = path
         }
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        cleanStore()
+    }
+    
+    // MARK: - Private
+    
+    private func cleanStore() {
+        /*
+        // Logic doesn't work properly. It should be done later.
+        let paths = try? FileManager.default.contentsOfDirectory(atPath: NSTemporaryDirectory())
+        let mapped = paths?.flatMap({
+            guard $0.contains(Constants.dataStorageName) else { return nil }
+            return $0
+        })
+        mapped?.forEach({
+            do {
+                try FileManager.default.removeItem(at: URL(fileURLWithPath: $0))
+            } catch {
+                XCTFail(error.localizedDescription)
+            }
+        })
+        */
+        
+        do {
+            let fm = FileManager.default
+            let directory = NSTemporaryDirectory()
+            
+            let items = try fm.contentsOfDirectory(atPath: directory)
+            for item in items {
+                let path = directory.appending(item)
+                try fm.removeItem(atPath: path)
+            }
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        
     }
 }
