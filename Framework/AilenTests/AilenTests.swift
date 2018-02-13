010d0560f13ecc12f46217d022fbbb198c62cb6e
Arkady Smirnov <arkady.smirnov@e-legion.com>
2018-02-13 11:07:43 +0300

    removed processors. removed excluded supporting coredata output and unit-tests.

--- Framework/AilenTests/AilenTests.swift
+++ Framework/AilenTests/AilenTests.swift
@@ -44,41 +44,41 @@ class AilenTests: XCTestCase {
         XCTAssertEqual(logger.outputs.count, 0)
     }
     
-    func testDefaultProcessor() {
-        guard let logger = logger else {
-            XCTFail("Logger uninitialized")
-            return
-        }
-        
-        let logExpectation = expectation(description: "Async logging wait")
-        
-        let output = TestingDefaultOutput()
-        logger.outputs = [output]
-        
-        logger.log(as: TestToken.UI, values: [Constants.msg])
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            XCTAssertEqual(logger.processors.count, 0)
-            XCTAssertEqual(output.messagesDisplayed, 1)
-            
-            let processor = TestingProcessor()
-            logger.processors = [processor]
-            
-            logger.log(as: TestToken.UI, values: [Constants.msg])
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                XCTAssertEqual(logger.processors.count, 1)
-                XCTAssertEqual(output.messagesDisplayed, 2)
-                
-                logger.processors.removeAll()
-                
-                logger.log(as: TestToken.UI, values: [Constants.msg])
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    XCTAssertEqual(logger.processors.count, 0)
-                    XCTAssertEqual(output.messagesDisplayed, 3)
-                    logExpectation.fulfill()
-                }
-            }
-        }
-        
-        waitForExpectations(timeout: 10)
-    }
+//    func testDefaultProcessor() {
+//        guard let logger = logger else {
+//            XCTFail("Logger uninitialized")
+//            return
+//        }
+//        
+//        let logExpectation = expectation(description: "Async logging wait")
+//        
+//        let output = TestingDefaultOutput()
+//        logger.outputs = [output]
+//        
+//        logger.log(as: TestToken.UI, values: [Constants.msg])
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//            XCTAssertEqual(logger.processors.count, 0)
+//            XCTAssertEqual(output.messagesDisplayed, 1)
+//            
+//            let processor = TestingProcessor()
+//            logger.processors = [processor]
+//            
+//            logger.log(as: TestToken.UI, values: [Constants.msg])
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//                XCTAssertEqual(logger.processors.count, 1)
+//                XCTAssertEqual(output.messagesDisplayed, 2)
+//                
+//                logger.processors.removeAll()
+//                
+//                logger.log(as: TestToken.UI, values: [Constants.msg])
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//                    XCTAssertEqual(logger.processors.count, 0)
+//                    XCTAssertEqual(output.messagesDisplayed, 3)
+//                    logExpectation.fulfill()
+//                }
+//            }
+//        }
+//        
+//        waitForExpectations(timeout: 10)
+//    }
 }
--- Source/Ailen.swift
+++ Source/Ailen.swift
@@ -13,61 +13,31 @@ public class Ailen {
     
     // MARK: - Properties
     
-    private var internalProcessors = [Processor]()
     public var outputs: [Output]
-    public var processors: [Processor] {
-        set {
-            var value = newValue
-            value.append(DebugProcessor())
-            internalProcessors = value
-        }
-        get {
-            return Array(internalProcessors.dropLast())
-        }
-    }
     
     // MARK: - Life cycle
     
-    public init(outputs: [Output] = [ConsoleOutput()], processors: [Processor] = []) {
+    public init(outputs: [Output] = [ConsoleOutput()]) {
         self.outputs = outputs
-        self.processors = processors
     }
     
     // MARK: - Public
     
-    public func log<TokenType: Token> (as token: TokenType, values: Any...)  {
-        self.log(as: token, tags: [EmptyTag](), values: values)
+    public func log<TokenType: Token> (as token: TokenType, value: Any)  {
+        self.log(as: token, tags: [EmptyTag](), value: value)
     }
     
-    public func log<Tag: RawRepresentable, TokenType: Token> (as token: TokenType, tags: [Tag], values: Any...) where Tag.RawValue == String {
+    public func log<Tag: RawRepresentable, TokenType: Token> (as token: TokenType, tags: [Tag], value: Any) where Tag.RawValue == String {
         
         perform(on: token.qos) {
-            let mapped: [Message<TokenType>] = values.flatMap {
-                value in
-                
-                guard let processed = self.convert(value, for: token) else { return nil }
-                
-                let tagsArray = tags.map { $0.rawValue }
-                return Message(token: token, tags: tagsArray, payload: processed)
-            }
-            self.outputs.forEach { self.log(mapped, in: $0) }
+            let tagsArray = tags.map { $0.rawValue }
+            let message = Message(token: token, tags: tagsArray, payload: value)
+            
+            self.outputs.forEach { $0.proccess(message) }
         }
     }
     
     // MARK: - Private
-    
-    private func log<TokenType: Token>(_ messages: [Message<TokenType>], in output: Output) {
-        messages.forEach(output.proccess)
-    }
-    
-    private func convert<TokenType: Token>(_ source: Any, for token: TokenType) -> String? {
-        for processor in internalProcessors {
-            if let processed = processor.process(token: token, object: source) {
-                return processed
-            }
-        }
-        return nil
-    }
     
     private func perform(on qos: Qos, block: @escaping () -> Void) {
         guard qos.queue != DispatchQueue.main && qos.isAsync else {
